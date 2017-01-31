9a562899c0bd81025573fe5812ba40772a98f132
Jan Gromeš <jgromes@users.noreply.github.com>
2017-01-31 17:22:01 +0100

    Update RohmMultiSensor.h

--- src/RohmMultiSensor.h
+++ src/RohmMultiSensor.h
@@ -10,6 +10,39 @@
   #include <Wire.h>
   
   #include "../sensors/utilities.cpp"
+
+  #ifdef INCLUDE_ALL_SENSORS
+    #warning "Do NOT use this include unless you are ABSOLUTELY sure what you are doing!"
+    #define INCLUDE_KX022_1020
+    #define INCLUDE_BM1383GLV
+    #define INCLUDE_BM1422GMV
+    #define INCLUDE_RPR_0521RS
+    #define INCLUDE_BH1745NUC
+    #define INCLUDE_BD7411G
+    #define INCLUDE_BD1020HFV
+    #define INCLUDE_ML8511A
+    #define INCLUDE_BH1790GLC
+  #endif
+  
+  #ifdef INCLUDE_ALL_1V8_SENSORS
+    #define INCLUDE_KX022_1020
+    #define INCLUDE_BM1383GLV
+    #define INCLUDE_BM1422GMV
+  #endif
+  
+  #ifdef INCLUDE_ALL_3V0_SENSORS
+    #define INCLUDE_KX022_1020
+    #define INCLUDE_BM1383GLV
+    #define INCLUDE_RPR_0521RS
+    #define INCLUDE_BH1745NUC
+    #define INCLUDE_BD1020HFV
+    #define INCLUDE_ML8511A
+  #endif
+  
+  #ifdef INCLUDE_ALL_5V0_SENSORS
+    #define INCLUDE_BD7411G
+    #define INCLUDE_BD1020HFV
+  #endif
   
   #ifdef INCLUDE_KX022_1020
     #include "../sensors/KX022_1020.cpp"
