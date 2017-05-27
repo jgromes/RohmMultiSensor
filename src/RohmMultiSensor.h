a84b181e757ad92d962685bca8cec84734e23d29
Jan Gromeš <jgromes@users.noreply.github.com>
2017-05-27 17:22:19 +0200

    Fixed folder structure

--- src/RohmMultiSensor.h
+++ src/RohmMultiSensor.h
@@ -9,10 +9,9 @@
 
   #include <Wire.h>
   
-  #include "../utilities/utilities.cpp"
+  #include "../extras/sensors/utilities.cpp"
 
   #ifdef INCLUDE_ALL_SENSORS
-    #warning "Do NOT use this include unless you are ABSOLUTELY sure what you are doing!"
     #define INCLUDE_KX022_1020
     #define INCLUDE_BM1383GLV
     #define INCLUDE_BM1422GMV
@@ -45,39 +44,39 @@
   #endif
   
   #ifdef INCLUDE_KX022_1020
-    #include "../utilities/KX022_1020.cpp"
+    #include "../extras/sensors/KX022_1020.cpp"
   #endif
 
   #ifdef INCLUDE_BM1383GLV
-    #include "../utilities/BM1383GLV.cpp"
+    #include "../extras/sensors/BM1383GLV.cpp"
   #endif
 
   #ifdef INCLUDE_BM1422GMV
-    #include "../utilities/BM1422GMV.cpp"
+    #include "../extras/sensors/BM1422GMV.cpp"
   #endif
 
   #ifdef INCLUDE_RPR_0521RS
-    #include "../utilities/RPR_0521RS.cpp"
+    #include "../extras/sensors/RPR_0521RS.cpp"
   #endif
 
   #ifdef INCLUDE_BH1745NUC
-    #include "../utilities/BH1745NUC.cpp"
+    #include "../extras/sensors/BH1745NUC.cpp"
   #endif
 
   #ifdef INCLUDE_BD7411G
-    #include "../utilities/BD7411G.cpp"
+    #include "../extras/sensors/BD7411G.cpp"
   #endif
 
   #ifdef INCLUDE_BD1020HFV
-    #include "../utilities/BD1020HFV.cpp"
+    #include "../extras/sensors/BD1020HFV.cpp"
   #endif
 
   #ifdef INCLUDE_ML8511A
-    #include "../utilities/ML8511A.cpp"
+    #include "../extras/sensors/ML8511A.cpp"
   #endif
   
   #ifdef INCLUDE_BH1790GLC
-    #include "../utilities/BH1790GLC.cpp"
+    #include "../extras/sensors/BH1790GLC.cpp"
   #endif
   
 #endif
